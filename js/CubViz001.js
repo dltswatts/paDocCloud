7cd77050781a8f0fbbdc08b879588555879aa06a
Dean <d.watts@grandviewcorp.com>
2019-12-02 16:52:53 -0500

    final update to version 2

--- js/CubViz001.js
+++ js/CubViz001.js
@@ -1,15 +1,6 @@
-<meta charset="utf-8">
-<style> /* set the CSS */
-
-.bar { fill: steelblue; }
-
-</style>
-<body>
-	
-<!-- load the d3.js library -->    	
-<script src="js/d3.v4.min.js"></script>
-<script>
-
+$(document).ready(function(){
+$.getScript('js/d3.v4.min.js', function()
+{
 // set the dimensions and margins of the graph
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = 900 - margin.left - margin.right,
@@ -25,35 +16,47 @@ var y = d3.scaleLinear()
 // append the svg object to the body of the page
 // append a 'group' element to 'svg'
 // moves the 'group' element to the top left margin
-var svg = d3.select("body").append("svg")
+var svg = d3.select("#CubViz").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform", 
           "translate(" + margin.left + "," + margin.top + ")");
 
+var tooltip = d3.select(".graphs")
+    .append("div")
+    .attr("class", "toolTip");
+    
 // get the data
-d3.csv("outputs/cubs.csv", function(error, data) {
+d3.csv("./outputs/cubs.csv", function(error, data) {
   if (error) throw error;
 
   // format the data
   data.forEach(function(d) {
-    d.sales = +d.sales;
+    d.dims = +d.dims;
   });
 
   // Scale the range of the data in the domains
-  x.domain(data.map(function(d) { return d.salesperson; }));
-  y.domain([0, d3.max(data, function(d) { return d.sales; })]);
+  x.domain(data.map(function(d) { return d.cubes; }));
+  y.domain([0, d3.max(data, function(d) { return d.dims; })]);
 
   // append the rectangles for the bar chart
   svg.selectAll(".bar")
       .data(data)
     .enter().append("rect")
       .attr("class", "bar")
-      .attr("x", function(d) { return x(d.salesperson); })
+      .attr("x", function(d) { return x(d.cubes); })
       .attr("width", x.bandwidth())
-      .attr("y", function(d) { return y(d.sales); })
-      .attr("height", function(d) { return height - y(d.sales); });
+      .attr("y", function(d) { return y(d.dims); })
+      .attr("height", function(d) { return height - y(d.dims); })
+      .on("mouseover", function(d){
+         tooltip
+            .style("left", d3.event.pageX - 50 + "px")
+            .style("top", d3.event.pageY - 100 + "px")
+            .style("display", "inline-block")
+            .html((d.legend) + "<BR>" + "Dimensions: " + (d.dims))
+       })
+       .on("mouseout", function(d){tooltip.style("display","none");});
 
   // add the x Axis
   svg.append("g")
@@ -65,6 +68,4 @@ d3.csv("outputs/cubs.csv", function(error, data) {
       .call(d3.axisLeft(y));
 
 });
-
-</script>
-</body>
+})});
--- outputs/paDoca.bat
+++ outputs/paDoca.bat
@@ -1,1 +1,1 @@
-dir /B "E:\TM1 Servers\GV Planning\Data\}Applications" > "E:\Program Files\ibm\cognos\tm1_64\webapps\tm1web\paDoc\outputs\applicationfolders.txt
+dir /B "E:\TM1 Servers\Food Distribution\Data\}Applications" > "E:\Program Files\ibm\cognos\tm1_64\webapps\tm1web\paDoc\outputs\applicationfolders.txt
